b1839fac9a7c067e400783f471de0698e48224eb
Roxy <75404941+TealSeer@users.noreply.github.com>
2026-02-26 21:21:37 -0500

    basic modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import AddModal from './AddModal'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const [isAddOpen, setIsAddOpen] = useState(false);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button type={'button'} onClick={() => setIsAddOpen(true)}>Click Me</button>
+      <AddModal isOpen={isAddOpen} setOpen={setIsAddOpen}>Hellooo</AddModal>
     </>
   )
 }
