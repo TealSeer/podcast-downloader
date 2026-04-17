26616c94deafa0b91c23b4ba540afe1a7e4e52e7
Roxy <75404941+TealSeer@users.noreply.github.com>
2026-04-16 23:19:28 -0500

    mark listened episodes

--- src/Episode.tsx
+++ src/Episode.tsx
@@ -3,14 +3,23 @@ import type { EpisodeData } from "./types";
 
 type EpisodeProps = {
   data: EpisodeData;
+  toggleListener: (id: string, newVal: boolean) => void;
+  listened: boolean;
 };
 
 const Episode = (props: EpisodeProps) => {
+  const { listened, toggleListener } = props;
   const { name, id, description, link } = props.data;
 
   return (
-    <details key={id} style={{ border: "1px solid" }}>
+    <details style={{ border: "1px solid" }}>
       <summary>
+        <input
+          type="checkbox"
+          title="Listened"
+          defaultChecked={listened}
+          onChange={(e) => toggleListener(id, e.target.checked)}
+        />
         <a href={link} download>
           {name}
         </a>
--- src/EpisodeList.tsx
+++ src/EpisodeList.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useLocalStorage } from "usehooks-ts";
 import Episode from "./Episode";
 import { getRSSEpisodes } from "./lib/rss";
 import type { EpisodeData, Podcast } from "./types";
@@ -11,6 +12,21 @@ const EpisodeList = (props: EpisodeListProps) => {
   const { activePodcast } = props;
   const [episodes, setEpisodes] = useState<EpisodeData[]>([]);
 
+  // An empty map gets deserialized as an object unless we explicitly handle it
+  const [listenedEpisodes, setListenedEpisodes] = useLocalStorage<
+    Map<string, boolean>
+  >(activePodcast.name, new Map<string, boolean>(), {
+    serializer: (val) => JSON.stringify(Object.fromEntries(val)),
+    deserializer: (val) =>
+      new Map<string, boolean>(Object.entries(JSON.parse(val))),
+  });
+
+  const toggleListened = (id: string, newVal: boolean) => {
+    const newMap = new Map<string, boolean>(listenedEpisodes);
+    newVal ? newMap.set(id, true) : newMap.delete(id);
+    setListenedEpisodes(newMap);
+  };
+
   useEffect(() => {
     const getEpisodes = async () => {
       const eps = await getRSSEpisodes(activePodcast.url);
@@ -22,7 +38,14 @@ const EpisodeList = (props: EpisodeListProps) => {
   return (
     <div>
       {episodes.map((episode) => {
-        return <Episode data={episode} key={episode.id} />;
+        return (
+          <Episode
+            data={episode}
+            key={episode.id}
+            toggleListener={toggleListened}
+            listened={listenedEpisodes.get(episode.id) ?? false}
+          />
+        );
       })}
     </div>
   );
